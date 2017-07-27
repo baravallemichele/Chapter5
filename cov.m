434cb3d9c9541dc338bef7d8c89af5265f36013e
baravallemichele <baravallemichele@gmail.com>
2017-07-27 13:54:16 +0200

    Delete cov.m

--- cov.m
+++
@@ -1,7 +0,0 @@
-function [ output_args ] = cov( input_args )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-
- output_args=std(input_args)/mean(input_args);
-end
-
